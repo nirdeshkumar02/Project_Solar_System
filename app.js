5f1623c02892eeb58b4bfe7bcbba19fc3ced269b
NIRDESH KUMAR SAINI <saini.nirdesh.kumar@gmail.com>
2024-04-11 10:57:27 +0530

    Added Service Container - Replica of Prod DB for Unit Testing

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-const connectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLOUD}/${process.env.DB_NAME}`
 
-mongoose.connect(connectionString, {
+mongoose.connect(`${process.env.MONGO_URI}`, {
+    user: `${process.env.MONGO_USERNAME}`,
+    pass: `${process.env.MONGO_PASSWORD}`,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
